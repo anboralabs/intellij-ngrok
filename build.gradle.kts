b73f43820f188f01605ed86c17a59e033aa72bba
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2022-05-21 09:40:10 -0500

    Migrated to kts.

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val publishingToken: String? = System.getenv("PUBLISH_TOKEN")
+
+plugins {
+    id("java")
+    kotlin("jvm") version "1.6.21"
+    id("org.jetbrains.intellij") version "1.5.3"
+}
+
+group = "co.anbora.labs"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.1")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.21")
+    implementation("com.github.alexdlaird:java-ngrok:1.5.6")
+}
+
+apply {
+    plugin("kotlin")
+    plugin("org.jetbrains.intellij")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version.set("LATEST-EAP-SNAPSHOT")
+}
+
+configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks {
+
+    patchPluginXml {
+        sinceBuild.set("211")
+        changeNotes.set(file("src/main/html/change-notes.html").inputStream().readBytes().toString(Charsets.UTF_8))
+        pluginDescription.set(file("src/main/html/description.html").inputStream().readBytes().toString(Charsets.UTF_8))
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "11"
+            languageVersion = "1.6"
+            apiVersion = "1.6"
+            freeCompilerArgs = listOf("-Xjvm-default=all")
+        }
+    }
+
+    publishPlugin {
+        token.set(publishingToken)
+    }
+}
