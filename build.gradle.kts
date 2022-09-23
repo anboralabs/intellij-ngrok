0d265a3664fb2470996894185607d520abb67cd9
Darwin Esteban Algarin Sarmiento <darwin.algarin@rappi.com>
2022-09-23 10:39:23 -0500

    Added intellij compatibility for 223 versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,19 +4,19 @@ val publishingToken: String? = System.getenv("PUBLISH_TOKEN")
 
 plugins {
     id("java")
-    kotlin("jvm") version "1.6.21"
+    id("org.jetbrains.kotlin.jvm") version "1.7.10"
     id("org.jetbrains.intellij") version "1.9.0"
 }
 
 group = "co.anbora.labs"
-version = "1.2.2"
+version = "1.2.3"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.4")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.21")
     implementation("com.github.alexdlaird:java-ngrok:1.5.6")
 }
@@ -28,28 +28,26 @@ apply {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2022.2")
+    version.set("LATEST-EAP-SNAPSHOT")
     plugins.set(listOf())
-}
-
-configure<JavaPluginExtension> {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
 }
 
 tasks {
 
     patchPluginXml {
-        sinceBuild.set("213.7172")
+        sinceBuild.set("223")
         changeNotes.set(file("src/main/html/change-notes.html").inputStream().readBytes().toString(Charsets.UTF_8))
         pluginDescription.set(file("src/main/html/description.html").inputStream().readBytes().toString(Charsets.UTF_8))
     }
 
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
     withType<KotlinCompile> {
         kotlinOptions {
-            jvmTarget = "11"
-            languageVersion = "1.6"
-            apiVersion = "1.6"
+            jvmTarget = "17"
             freeCompilerArgs = listOf("-Xjvm-default=all")
         }
     }
