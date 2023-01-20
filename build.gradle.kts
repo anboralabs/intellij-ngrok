c7b514d21c3511c81bfe3a8475f4166840ad6cdc
Darwin Esteban Algarin Sarmiento <darwin.algarin@rappi.com>
2023-01-20 08:56:26 -0500

    fix casting type.

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("223")
+        untilBuild.set("223.*")
         changeNotes.set(file("src/main/html/change-notes.html").inputStream().readBytes().toString(Charsets.UTF_8))
         pluginDescription.set(file("src/main/html/description.html").inputStream().readBytes().toString(Charsets.UTF_8))
     }
