d92d58310506c66e65a6631c6a809d4d6442506b
Darwin Algarin <dalgarins@live.com>
2024-03-23 16:32:04 -0500

    change from since build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ intellij {
 tasks {
 
     patchPluginXml {
-        sinceBuild.set("232")
+        sinceBuild.set("233")
         untilBuild.set("241.*")
         changeNotes.set(file("src/main/html/change-notes.html").inputStream().readBytes().toString(Charsets.UTF_8))
         pluginDescription.set(file("src/main/html/description.html").inputStream().readBytes().toString(Charsets.UTF_8))
