5ff1dba83f076d3ea54b3649fb4ecec23a3fe521
Darwin Algarin <dalgarins@live.com>
2024-04-28 09:57:30 -0500

    fix copy issue, now only copy values.

--- src/main/kotlin/co/anbora/labs/ngrok/remote/server/components/ValuesTable.kt
+++ src/main/kotlin/co/anbora/labs/ngrok/remote/server/components/ValuesTable.kt
@@ -1,9 +1,17 @@
 package co.anbora.labs.ngrok.remote.server.components
 
 import com.intellij.ui.table.JBTable
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import java.awt.event.KeyEvent
+import javax.swing.JComponent
+import javax.swing.KeyStroke
 import javax.swing.table.DefaultTableModel
 
-class ValuesTable(values: MutableMap<String, String?>) : JBTable() {
+
+class ValuesTable(values: MutableMap<String, String?>) : JBTable(), ActionListener {
     companion object {
         private const val NAME_COLUMN_TITLE = "Name"
         private const val VALUE_COLUMN_TITLE = "Value"
@@ -11,6 +19,9 @@ class ValuesTable(values: MutableMap<String, String?>) : JBTable() {
 
     private val keyRowMap: MutableMap<String, Int> = mutableMapOf()
     private val tableModel: DefaultTableModel = DefaultTableModel(arrayOf(NAME_COLUMN_TITLE, VALUE_COLUMN_TITLE), 0)
+
+    private val copy: KeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK, false)
+    private val copyMac: KeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.META_MASK, false)
 
     init {
         var index = 0
@@ -21,6 +32,9 @@ class ValuesTable(values: MutableMap<String, String?>) : JBTable() {
         }
 
         model = tableModel
+
+        this.registerKeyboardAction(this, copy, JComponent.WHEN_FOCUSED)
+        this.registerKeyboardAction(this, copyMac, JComponent.WHEN_FOCUSED)
     }
 
     override fun isCellEditable(row: Int, column: Int): Boolean = false
@@ -47,5 +61,12 @@ class ValuesTable(values: MutableMap<String, String?>) : JBTable() {
             tableModel.removeRow(row)
             keyRowMap.remove(deletedRow)
         }
+    }
+
+    override fun actionPerformed(e: ActionEvent?) {
+        val sbf = selectedRows.map { this.getValueAt(it, 1) }.joinToString()
+        val selection = StringSelection(sbf)
+        val clipBoard = Toolkit.getDefaultToolkit().systemClipboard
+        clipBoard.setContents(selection, selection)
     }
 }
