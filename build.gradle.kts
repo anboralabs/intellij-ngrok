eb42e7c4541b1013ebf1269a41282083b5cce0e1
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2026-09-06 12:36:41 +0000

    upgrade sdk to api 263

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ fun environment(key: String) = providers.environmentVariable(key)
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.3.20"
-    id("org.jetbrains.intellij.platform") version "2.16.0"
+    id("org.jetbrains.intellij.platform") version "2.18.1"
 }
 
 group = properties("pluginGroup").get()
